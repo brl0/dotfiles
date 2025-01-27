3ed028e335135464622d2e08dec4de2943569eb8
Brian Larsen <B_R_L@hotmail.com>
2025-01-27 10:46:41 -0600

    Update link config and creation

--- .files/scripts/update_links.sh
+++ .files/scripts/update_links.sh
@@ -4,8 +4,21 @@
 
 echo 'script begin'
 
+if [[ -n "$1" ]]; then
+    CONFIG_FILE="$1"
+else
+    script_dir=$(dirname "$0")
+    CONFIG_FILE="$script_dir/../config/dotfiles.yaml"
+fi
+
 pushd ~ || exit
-xargs -a ~/dotfiles/.files/config/wsl_links.txt -i -r echo {} | xargs -r -n 2 ln -s
+
+# xargs -a ~/dotfiles/.files/config/wsl_links.txt -i -r echo {} | xargs -r -n 2 ln -s
+
+while read -r key value; do
+    ln -s "$value" "$HOME/$key"
+done < <(yq eval '.symlinks | to_entries | .[] | "\(.key) \(.value)"' "$CONFIG_FILE")
+
 popd || exit
 
 echo 'script complete'
