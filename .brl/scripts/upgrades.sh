3b4addf102daf4480e515b6abbc811619e2d2d56
Brian Larsen <B_R_L@hotmail.com>
2026-09-04 18:14:20 -0500

    Update upgrades

--- .brl/scripts/upgrades.sh
+++ .brl/scripts/upgrades.sh
@@ -8,14 +8,14 @@ sudo apt auto-remove -y
 
 brew update
 brew upgrade "$(brew outdated --cask --greedy)"
-brew upgrade
+brew upgrade -y
 brew cu -a -y --cleanup
 brew cleanup
-brew unlink python
-brew unlink python3.8
-brew unlink python3.9
-brew unlink python3.10
-brew unlink python3.11
+# brew unlink python
+# brew unlink python3.8
+# brew unlink python3.9
+# brew unlink python3.10
+# brew unlink python3.11
 
 topgrade --disable conda --show-skipped -v -y
 mamba update --no-banner -n base -y --all
